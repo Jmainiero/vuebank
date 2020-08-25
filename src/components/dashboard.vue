257362c62e4e587adea11729699e5e502df64fe8
Jmainiero <jcmainiero@gmail.com>
2020-08-25 08:28:18 -0400

    Update dashboard interface

--- src/components/dashboard.vue
+++ src/components/dashboard.vue
@@ -2,49 +2,57 @@
   <div>
     <!-- <appNav /> -->
     <div class="accountOverview">
-      <div class="row">
-        <div class="accountOverview--acctype">
-          <h1>Checking</h1>
-        </div>
-        <div class="col-1-of-3">
-          <div class="feature-box">
-            <p class="p-ck-title p-ck-title--balance">Checking</p>
-            <p class="p-ck---balance">$7,077.21</p>
+      <div class="tab">
+        <button class="tablinks" @click="openCity(this,'checking')">Checking</button>
+        <button class="tablinks" @click="openCity(this,'savings')">Savings</button>
+      </div>
+      <div id="checking" class="tabcontent" style="display:block;">
+        <div class="row">
+          <div class="accountOverview--acctype">
+            <h1>Checking</h1>
           </div>
-        </div>
-        <div class="col-1-of-3">
-          <div class="feature-box">
-            <p class="p-ck-title p-ck-title--startbalance">Starting Balance</p>
-            <p class="p-ck--startbalance">$6,981.08</p>
+          <div class="col-1-of-3">
+            <div class="feature-box">
+              <p class="p-ck-title p-ck-title--balance">Checking</p>
+              <p class="p-ck---balance">$7,077.21</p>
+            </div>
           </div>
-        </div>
-        <div class="col-1-of-3">
-          <div class="feature-box">
-            <p class="p-ck-title p-ck-title--availbal">Available Balance</p>
-            <p class="p-ck--availbal">$5,451.79</p>
+          <div class="col-1-of-3">
+            <div class="feature-box">
+              <p class="p-ck-title p-ck-title--startbalance">Starting Balance</p>
+              <p class="p-ck--startbalance">$6,981.08</p>
+            </div>
+          </div>
+          <div class="col-1-of-3">
+            <div class="feature-box">
+              <p class="p-ck-title p-ck-title--availbal">Available Balance</p>
+              <p class="p-ck--availbal">$5,451.79</p>
+            </div>
           </div>
         </div>
       </div>
-      <div class="row">
-        <div class="accountOverview--acctype">
-          <h1>Savings</h1>
-        </div>
-        <div class="col-1-of-3">
-          <div class="feature-box">
-            <p class="p-sv-title p-sv-title--availbal">Available Balance</p>
-            <p class="p-sv--availbal">$5,451.79</p>
+      <div id="savings" class="tabcontent">
+        <div class="row">
+          <div class="accountOverview--acctype">
+            <h1>Savings</h1>
           </div>
-        </div>
-        <div class="col-1-of-3">
-          <div class="feature-box">
-            <p class="p-sv-title p-sv-title--availbal">Available Balance</p>
-            <p class="p-sv--availbal">$5,451.79</p>
+          <div class="col-1-of-3">
+            <div class="feature-box">
+              <p class="p-sv-title p-sv-title--availbal">Available Balance</p>
+              <p class="p-sv--availbal">$5,451.79</p>
+            </div>
           </div>
-        </div>
-        <div class="col-1-of-3">
-          <div class="feature-box">
-            <p class="p-sv-title p-sv-title--availbal">Available Balance</p>
-            <p class="p-ck--availbal">$5,451.79</p>
+          <div class="col-1-of-3">
+            <div class="feature-box">
+              <p class="p-sv-title p-sv-title--availbal">Available Balance</p>
+              <p class="p-sv--availbal">$5,451.79</p>
+            </div>
+          </div>
+          <div class="col-1-of-3">
+            <div class="feature-box">
+              <p class="p-sv-title p-sv-title--availbal">Available Balance</p>
+              <p class="p-ck--availbal">$5,451.79</p>
+            </div>
           </div>
         </div>
       </div>
@@ -58,6 +66,20 @@ export default {
   computed: {
     loggedIn() {
       return this.$store.getters.grabStatus;
+    },
+  },
+  methods: {
+    openCity(evt, cityName) {
+      // Declare all variables
+      var i, tabcontent;
+
+      // Get all elements with class="tabcontent" and hide them
+      tabcontent = document.getElementsByClassName("tabcontent");
+      for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+      }
+      // Show the current tab, and add an "active" class to the button that opened the tab
+      document.getElementById(cityName).style.display = "block";
     },
   },
 };
@@ -74,24 +96,24 @@ export default {
     rgba(#254e58, 0.9),
     rgba(#112d32, 1)
   );
-  // background: linear-gradient(
-  //   to bottom right,
-  //   rgba(#3b6978, 0.9),
-  //   rgba(#204051, 1)
-  // );
   border-radius: 3px;
-  padding: 5rem;
+  // padding: 5rem;
   margin: 0rem 10rem;
   color: $color-white;
   &--acctype {
-    // display: inline-block;
-    // width: 60%;
     & h1 {
       font-size: 3rem;
       font-weight: 300;
       margin-bottom: 1rem;
       text-transform: uppercase;
     }
+  }
+  a,
+  a:visited {
+    color: inherit;
+    text-decoration: none;
+    display: block;
+    margin-bottom: 2rem;
   }
 }
 
@@ -121,6 +143,9 @@ export default {
     font-size: inherit;
     font-weight: 700;
   }
+  &:hover {
+    transform: scale(1.1);
+  }
 }
 .p-sv-title,
 .p-ck-title {
@@ -128,5 +153,42 @@ export default {
   margin-bottom: 2rem;
   color: $color-grey-light-2;
   font-weight: 300 !important;
+}
+
+/* Style the tab */
+.tab {
+  overflow: hidden;
+  background-color: transparent;
+}
+
+/* Style the buttons that are used to open the tab content */
+.tab button {
+  background-color: inherit;
+  float: left;
+  border: none;
+  outline: none;
+  cursor: pointer;
+  padding: 14px 16px;
+  transition: 0.3s;
+  font-size: 1.5rem !important;
+  font-family: inherit;
+  color: $color-grey-light-2;
+  text-transform: uppercase;
+  border-bottom: 1px solid $color-grey-dark-2;
+
+  &:hover {
+    color: #32e0c4;
+    transform: scale(1.2);
+    margin: 0 1rem 0 2rem;
+  }
+  &:active {
+    background-color: #ccc;
+  }
+}
+/* Style the tab content */
+.tabcontent {
+  display: none;
+  padding: 6px 12px;
+  border-top: none;
 }
 </style>
